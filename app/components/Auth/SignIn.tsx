550035a898979f25c9041aa71d4f2344537c06b0
David <9012667+dhpar@users.noreply.github.com>
2025-09-23 18:05:29 -0500

    Updates 9 19 25 (#14)
    * Refactor time window checks and improve section validation logic
    
    * feat: Implementing next auth.

---
+++ app/components/Auth/SignIn.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function SignIn() {
+    const { data: session } = useSession();
+
+    if (session?.user) {
+        return (
+            <>
+                <p>Signed in as {session.user.email}</p>
+                <button onClick={() => signOut()} className="py-2 px-4 mr-4 text-slate-50 border-slate-50 border-solid border-2 rounded-lg hover:text-slate-900  hover:bg-slate-50">Sign out</button>
+            </>
+        );
+    }
+    return (
+        <>
+            {/* <p>Not signed in</p> */}
+            <button onClick={() => signIn()} className="py-2 px-4 mr-4 text-slate-50 border-slate-50 border-solid border-2 rounded-lg hover:text-slate-900  hover:bg-slate-50">Sign in</button>
+        </>
+    );
+}
