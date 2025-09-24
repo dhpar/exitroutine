a648ef81b0d5e31c11a51457e6d1a8c9b3fb14cb
David <davhern@gmail.com>
2025-09-24 13:21:40 -0500

    fix: Update user session display to show address instead of email and clean up NextAuth options

--- app/components/Auth/SignIn.tsx
+++ app/components/Auth/SignIn.tsx
@@ -7,7 +7,7 @@ export default function SignIn() {
     if (session?.user) {
         return (
             <>
-                <p>Signed in as {session.user.email}</p>
+                <p>Signed in as {session.user?.address}</p>
                 <button onClick={() => signOut()} className="py-2 px-4 mr-4 text-slate-50 border-slate-50 border-solid border-2 rounded-lg hover:text-slate-900  hover:bg-slate-50">Sign out</button>
             </>
         );
