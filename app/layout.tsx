d6ebee5acbd4ace5b7fdefbeaba46d01785ebb23
David <9012667+dhpar@users.noreply.github.com>
2025-09-24 15:52:41 -0500

    Updates 9 19 25 (#19)
    * Refactor time window checks and improve section validation logic
    
    * feat: Implementing next auth.
    
    * fix: Replace anchor tag with Link component for sign-in navigation
    
    * fix: Correct pre-commit trigger to run build on commit
    
    * fix: Update NextAuth export and add TypeScript definitions for session
    
    * fix: Update user session display to show address instead of email and clean up NextAuth options
    
    * feat: Add Vercel Analytics integration and update package dependencies
    
    * fix: Update user session display to show name instead of address and enhance TypeScript definitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({children}: Readonly<RootLayoutProps>) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
+      <Analytics />
     </html>
   );
 }
