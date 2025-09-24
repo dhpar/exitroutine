23beff6378ab53f586f103fed099e600a90bca54
David <9012667+dhpar@users.noreply.github.com>
2025-09-24 16:06:15 -0500

    Updates 9 19 25 (#20)
    * Refactor time window checks and improve section validation logic
    
    * feat: Implementing next auth.
    
    * fix: Replace anchor tag with Link component for sign-in navigation
    
    * fix: Correct pre-commit trigger to run build on commit
    
    * fix: Update NextAuth export and add TypeScript definitions for session
    
    * fix: Update user session display to show address instead of email and clean up NextAuth options
    
    * feat: Add Vercel Analytics integration and update package dependencies
    
    * fix: Update user session display to show name instead of address and enhance TypeScript definitions
    
    * fix: Correcting signin info.

--- app/components/Auth/SignIn.tsx
+++ app/components/Auth/SignIn.tsx
@@ -7,14 +7,14 @@ export default function SignIn() {
     if (session?.user) {
         return (
             <>
-                <p>Signed in as {session.user?.address}</p>
+                <p className="flex">Signed in as {session.user?.name}</p>
                 <button onClick={() => signOut()} className="py-2 px-4 mr-4 text-slate-50 border-slate-50 border-solid border-2 rounded-lg hover:text-slate-900  hover:bg-slate-50">Sign out</button>
             </>
         );
     }
     return (
         <>
-            {/* <p>Not signed in</p> */}
+            <p>Not signed in</p>
             <button onClick={() => signIn()} className="py-2 px-4 mr-4 text-slate-50 border-slate-50 border-solid border-2 rounded-lg hover:text-slate-900  hover:bg-slate-50">Sign in</button>
         </>
     );
